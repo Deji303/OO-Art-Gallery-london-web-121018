406eddacd3286d39819648467099c5313f91ebe4
github username <github email address>
2018-12-16 15:50:47 +0000

    learn submit

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,48 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |painting|
+      painting.artist == self
+    end
+  end
+
+  def galleries
+    paintings.map do |painting|
+      painting.gallery
+    end
+  end
+
+  def cities
+    galleries.map do |gallery|
+      gallery.city
+    end
+  end
+
+  def self.total_experience
+    @@all.map {|artist| artist.years_experience }.reduce(:+)
+  end
+
+  def self.most_prolific
+    @@all.max_by do |artist|
+      (artist.paintings.to_f/artist.years_experience).round(2)
+    end
+  end
+
+  def create_painting(title,price,gallery)
+    Painting.new(title, price, self, gallery)
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,15 @@
 require_relative '../config/environment.rb'
 
-
-
+nyc = Gallery.new('The tent','NYC')
+ldn = Gallery.new('798','London')
+cq = Gallery.new('Arthouse 1','Chongqing')
+john = Artist.new('johnny',78)
+jacob = Artist.new('tommyd',57)
+jingle = Artist.new('jimbob',44)
+mona = Painting.new("Mona", 1000,john,nyc)
+lisa = Painting.new("Lisa", 2000,jacob,ldn)
+lake = Painting.new("Laky", 3000,jingle,cq)
+sea = Painting.new("Seay", 4000,mona,nyc)
 
 binding.pry
 
